ac8f1681da5d144e36de77bc3e332b5a41b1d290
Rodrigo das Gracas Pereira <rodrigogr.pereira@gmail.com>
2024-04-06 21:01:46 -0300

    quatro commit

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,11 @@
             //alert("Funcionado!");
             */
            $('nav.mobile').click(function(){
-                
-           });
+                var listaMenu = $('nav.mobile ul');
+                if(listaMenu.is(':hidden')== true){
+                listaMenu.fadeIn(); 
+                }else{
+                    listaMenu.fadeOut();
+                }
+           })
         })
